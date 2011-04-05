38887d1abe36690d82290bfd03e9bba8052c6ee2
HATAYAMA Daisuke <d.hatayama@jp.fujitsu.com>
2011-04-05 17:22:39 +0900

    Update Copyright declaration

--- src/libgcore/gcore_elf_struct.c
+++ src/libgcore/gcore_elf_struct.c
@@ -1,3 +1,18 @@
+/* gcore_elf_struct.c -- core analysis suite
+ *
+ * Copyright (C) 2011 FUJITSU LIMITED
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #include <defs.h>
 #include "gcore_defs.h"
 
